ccc20596bf5064f5128a3428dddbfd8d1eacb6eb
Miloš Jovičić <milos.jovicic@planetsoft.eu>
2023-04-19 08:24:50 +0200

    init

---
+++ src/main/java/rs/optaplannerjobschedule/OptaPlannerJobScheduleApplication.java
@@ -0,0 +1,13 @@
+package rs.optaplannerjobschedule;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OptaPlannerJobScheduleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OptaPlannerJobScheduleApplication.class, args);
+    }
+
+}
